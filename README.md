cb908d9e8f0b324ad6c3818aa8be9951955e130b
Tianhao Zhou <tianhaoz@umich.edu>
2020-04-02 16:48:01 -0700

    dev: add social media (#7)

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-[![Gitpod Ready-to-Code](https://img.shields.io/badge/Gitpod-Ready--to--Code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/tianhaoz95/codeNboba) 
+[![Gitpod Ready-to-Code](https://img.shields.io/badge/Gitpod-Ready--to--Code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/tianhaoz95/codeNboba)
 
 # codeNboba
+
 Personal site. What's better to describe myself than code &amp; boba?
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,39 +1,95 @@
 module.exports = {
   locales: {
-    '/': {
-      lang: 'en-US',
-      title: 'Jackson Zhou',
-      description: 'Home for all my projects'
+    "/": {
+      lang: "en-US",
+      title: "Jackson Zhou",
+      description: "Home for all my projects",
     },
-    '/zh/': {
-      lang: 'zh-CN',
-      title: '天豪同学个人主页',
-      description: '博客项目汇总'
-    }
+    "/zh/": {
+      lang: "zh-CN",
+      title: "天豪同学个人主页",
+      description: "博客项目汇总",
+    },
   },
   themeConfig: {
-    logo: 'avatar.png',
+    logo: "avatar.png",
     locales: {
-      '/': {
-        selectText: 'Languages',
-        label: 'English',
-        sidebar: [
-          ['/', 'Home']
-        ],
+      "/": {
+        selectText: "Languages",
+        label: "English",
+        sidebar: [["/", "Home"]],
         nav: [
-          { text: 'GitHub', link: 'https://github.com/tianhaoz95', target:'_blank' }
-        ]
+          {
+            text: "Social Media",
+            items: [
+              {
+                text: "Facebook",
+                link: "https://www.facebook.com/tianhaozhou95/",
+                target: "_blank",
+              },
+              {
+                text: "LinkedIn",
+                link: "https://www.linkedin.com/in/tianhaoz/",
+                target: "_blank",
+              },
+              {
+                text: "Twitter",
+                link: "https://twitter.com/TheSWE2",
+                target: "_blank",
+              },
+              {
+                text: "YouTube",
+                link:
+                  "https://www.youtube.com/channel/UCY13XGU7-3mYz2n1NzV4oGw",
+                target: "_blank",
+              },
+            ],
+          },
+          {
+            text: "GitHub",
+            link: "https://github.com/tianhaoz95",
+            target: "_blank",
+          },
+        ],
       },
-      '/zh/': {
-        selectText: '选择语言',
-        label: '简体中文',
-        sidebar: [
-          ['/zh/', '主页']
-        ],
+      "/zh/": {
+        selectText: "选择语言",
+        label: "简体中文",
+        sidebar: [["/zh/", "主页"]],
         nav: [
-          { text: 'GitHub', link: 'https://github.com/tianhaoz95', target:'_blank' }
-        ]
-      }
-    }
-  }
-}
+          {
+            text: "社媒号",
+            items: [
+              {
+                text: "脸书",
+                link: "https://www.facebook.com/tianhaozhou95/",
+                target: "_blank",
+              },
+              {
+                text: "领英",
+                link: "https://www.linkedin.com/in/tianhaoz/",
+                target: "_blank",
+              },
+              {
+                text: "推特",
+                link: "https://twitter.com/TheSWE2",
+                target: "_blank",
+              },
+              {
+                text: "油管",
+                link:
+                  "https://www.youtube.com/channel/UCY13XGU7-3mYz2n1NzV4oGw",
+                target: "_blank",
+              },
+            ],
+          },
+          {
+            text: "GitHub",
+            link: "https://github.com/tianhaoz95",
+            target: "_blank",
+          },
+        ],
+      },
+    },
+  },
+};
