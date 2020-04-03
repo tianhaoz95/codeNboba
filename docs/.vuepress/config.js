16bb2e34d2368a6be83a512da85e123cbe3b23b4
Tianhao Zhou <tianhaoz@umich.edu>
2020-04-03 06:28:47 +0000

    add more plugin

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,34 @@
 module.exports = {
+  head: [
+    ["link", { rel: "icon", href: "/icon/favicon.ico" }],
+    ["link", { rel: "manifest", href: "/manifest.json" }],
+    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
+    ],
+    [
+      "link",
+      { rel: "apple-touch-icon", href: "/icons/apple-touch-icon-152x152.png" },
+    ],
+    [
+      "link",
+      {
+        rel: "mask-icon",
+        href: "/icons/safari-pinned-tab.svg",
+        color: "#3eaf7c",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "msapplication-TileImage",
+        content: "/icons/msapplication-icon-144x144.png",
+      },
+    ],
+    ["meta", { name: "msapplication-TileColor", content: "#000000" }],
+  ],
   locales: {
     "/": {
       lang: "en-US",
@@ -15,6 +45,14 @@ module.exports = {
     "@vuepress/back-to-top",
     "@vuepress/nprogress",
     "@vuepress/medium-zoom",
+    "@vuepress/pwa",
+    "@vuepress/active-header-links",
+    [
+      "@vuepress/google-analytics",
+      {
+        ga: "UA-162676517-1",
+      },
+    ],
     [
       "@vuepress/blog",
       {
@@ -29,7 +67,7 @@ module.exports = {
     ],
   ],
   themeConfig: {
-    logo: "avatar.png",
+    logo: "/icons/avatars/avatar.png",
     sidebar: "auto",
     repo: "tianhaoz95/codeNboba",
     editLinks: true,
